d84665f1e5df1e8a899c609e15c899e1c1dc7efd
avulindlela <69433638+avulindlela@users.noreply.github.com>
2021-03-20 23:34:59 +0200

    20 March 2021 Men edit

--- javascript/main.js
+++ javascript/main.js
@@ -1,8 +1,15 @@
-const parallax = document.getElementById('content');
+const parallax1 = document.getElementById('home-content');
 
 window.addEventListener('scroll', function(){
-    let offset = window.pageYOffset;
-    parallax.style.backgroundPositionX = -offset * 0.2 + "px";
+    let offset1 = window.pageYOffset;
+    parallax1.style.backgroundPositionY = -offset1 * 0.130 + "px";
     
 })
     
+const parallax2 = document.getElementById('men-content');
+
+window.addEventListener('scroll', function(){
+    let offset2 = window.pageYOffset;
+    parallax2.style.backgroundPositionY = -offset2 * 0.2 + "px";
+    
+})
